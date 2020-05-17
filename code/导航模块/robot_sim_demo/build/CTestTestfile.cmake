1cea61529e89bd3326ddc4a758c6266b9875d995
zt <zoutao@buaa.edu.cn>
2020-05-17 18:15:47 +0800

    导航--建图雷达方面和虚拟环境上传
    导航--建图雷达方面和虚拟环境上传

---
+++ code/导航模块/robot_sim_demo/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/zt/demo_ws/src/robot_sim_demo/src
+# Build directory: /home/zt/demo_ws/src/robot_sim_demo/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
