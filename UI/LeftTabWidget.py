8ad06020e338ea368b75cb86c3031420f786c77b
zzxbh <47728320+zzxbh@users.noreply.github.com>
2020-05-21 09:50:19 +0800

    添加UI导航页面部分
    运行时注意修改button2_1click到button2_6click中路径部分

--- UI/LeftTabWidget.py
+++ UI/LeftTabWidget.py
@@ -128,9 +128,116 @@ class LeftTabWidget(QWidget):
                     self.item.setTextAlignment(Qt.AlignCenter)  # 居中显示
 
 
+            elif i == 2:
+                self.centralWidget2=QtWidgets.QWidget()
+                self.centralWidget2.setStyleSheet('''background:#F0FFFF;border-width:0;''');
+                self.layout2 = QtWidgets.QGridLayout()  # 创建左侧部件的网格布局层
+                self.centralWidget2.setLayout(self.layout2)
 
+                self.button2_7 = QtWidgets.QPushButton(self.centralWidget2)
+                self.button2_7.setObjectName("button7")
+                self.button2_7.setText("启动手柄")
+                self.button2_7.clicked.connect(self.button2_7click)
+                self.button2_7.setStyleSheet('''QPushButton{background:#1E90FF;border-radius:20px;font-family:等线;
+                                               font-size:18px;color:white}QPushButton:hover{background:#EEDC82;}''')
+                self.button2_7.setFixedHeight(40)
 
-			# '硬件设置'
+                self.button2_1 = QtWidgets.QPushButton(self.centralWidget2)
+                self.button2_1.setObjectName("button1")
+                self.button2_1.setText("构建地图")
+                self.button2_1.clicked.connect(self.button2_1click)
+                self.button2_1.setStyleSheet('''QPushButton{background:#1E90FF;border-radius:20px;font-family:等线;
+                                               font-size:18px;color:white}QPushButton:hover{background:#EEDC82;}''')
+                self.button2_1.setFixedHeight(40)
+
+                self.button2_2 = QtWidgets.QPushButton(self.centralWidget2)
+                self.button2_2.setObjectName("button2")
+                self.button2_2.setText("保存地图")
+                self.button2_2.clicked.connect(self.button2_2click)
+                self.button2_2.setStyleSheet('''QPushButton{background:#1E90FF;border-radius:20px;font-family:等线;
+                                               font-size:18px;color:white}QPushButton:hover{background:#EEDC82;}''')
+                self.button2_2.setFixedHeight(40)
+
+                self.button2_3 = QtWidgets.QPushButton(self.centralWidget2)
+                self.button2_3.setObjectName("button3")
+                self.button2_3.setText("设立航点")
+                self.button2_3.clicked.connect(self.button2_3click)
+                self.button2_3.setStyleSheet('''QPushButton{background:#1E90FF;border-radius:20px;font-family:等线;
+                                               font-size:18px;color:white}QPushButton:hover{background:#EEDC82;}''')
+                self.button2_3.setFixedHeight(40)
+
+                self.button2_4 = QtWidgets.QPushButton(self.centralWidget2)
+                self.button2_4.setObjectName("button4")
+                self.button2_4.setText("保存航点")
+                self.button2_4.clicked.connect(self.button2_4click)
+                self.button2_4.setStyleSheet('''QPushButton{background:#1E90FF;border-radius:20px;font-family:等线;
+                                               font-size:18px;color:white}QPushButton:hover{background:#EEDC82;}''')
+                self.button2_4.setFixedHeight(40)
+
+                self.button2_5 = QtWidgets.QPushButton(self.centralWidget2)
+                self.button2_5.setObjectName("button5")
+                self.button2_5.setText("开始导航")
+                self.button2_5.clicked.connect(self.button2_5click)
+                self.button2_5.setStyleSheet('''QPushButton{background:#1E90FF;border-radius:20px;font-family:等线;
+                                               font-size:18px;color:white}QPushButton:hover{background:#EEDC82;}''')
+                self.button2_5.setFixedHeight(40)
+
+                self.comboBox2 = QtWidgets.QComboBox(self.centralWidget2)
+                self.comboBox2.setObjectName("comboBox")
+                self.comboBox2.setStyleSheet('''QComboBox{background:#1E90FF;border-radius:10px;font-family:等线;
+                                               font-size:18px;color:white}QComboBox:hover{background:#EEDC82;}''')
+                self.comboBox2.setFixedHeight(40)
+
+                self.button2_6 = QtWidgets.QPushButton(self.centralWidget2)
+                self.button2_6.setObjectName("button6")
+                self.button2_6.setText("G O !")
+                self.button2_6.clicked.connect(self.button2_6click)
+                self.button2_6.setStyleSheet('''QPushButton{background:#1E90FF;border-radius:20px;font-family:等线;
+                                               font-size:18px;color:white}QPushButton:hover{background:#EEDC82;}''')
+                self.button2_6.setFixedHeight(40)
+
+                self.label2_1 = QtWidgets.QLabel()
+                self.label2_1.setTextFormat(QtCore.Qt.AutoText)
+                self.label2_1.setText("导航")
+                self.label2_1.setStyleSheet('''color:1E90FF;font-size:23px;background:#F0FFFF;
+                                                font-family:等线;''')
+                self.label2_1.setAlignment(Qt.AlignCenter)
+
+                self.label2_2 = QtWidgets.QLabel()
+                self.label2_2.setTextFormat(QtCore.Qt.AutoText)
+                self.label2_2.setText("")
+                self.label2_2.setAlignment(Qt.AlignCenter)
+                
+                self.layout2.setColumnStretch(0, 1)
+                self.layout2.setColumnStretch(1, 2)
+                self.layout2.setColumnStretch(2, 2)
+                self.layout2.setColumnStretch(3, 1)            
+                self.layout2.setColumnStretch(4, 2)
+                self.layout2.setColumnStretch(5, 2)
+                self.layout2.setColumnStretch(6, 1)
+                self.layout2.setRowStretch(0,2)
+                self.layout2.setRowStretch(1,2)
+                self.layout2.setRowStretch(2,2)
+                self.layout2.setRowStretch(3,2)
+                self.layout2.setRowStretch(4,2)
+                self.layout2.setRowStretch(5,2)
+                self.layout2.setRowStretch(6,2)
+                self.layout2.setRowStretch(7,2)
+                self.layout2.setRowStretch(8,2)
+                self.layout2.setHorizontalSpacing(5)
+                self.layout2.setVerticalSpacing(5)
+                self.layout2.addWidget(self.label2_1, 0,0,1,7)
+                self.layout2.addWidget(self.button2_7, 2,4,1,2)
+                self.layout2.addWidget(self.button2_1, 2,1,1,2)
+                self.layout2.addWidget(self.button2_2, 4,1,1,2)
+                self.layout2.addWidget(self.button2_3, 4,4,1,2)
+                self.layout2.addWidget(self.button2_4, 6,1,1,2)
+                self.layout2.addWidget(self.button2_5, 6,4,1,2)
+                self.layout2.addWidget(self.button2_6, 8,4,1,2)
+                self.layout2.addWidget(self.comboBox2, 8,1,1,2)
+                self.layout2.addWidget(self.label2_2, 9,1,1,7)
+                self.right_widget.addWidget(self.centralWidget2)
+# '硬件设置'
             elif i == 7:
                 self.centralWidget7 = QtWidgets.QWidget()
                 self.centralWidget7.setStyleSheet('''background:#F0FFFF;border-width:0;''');
@@ -367,17 +474,66 @@ class LeftTabWidget(QWidget):
                     self.centralWidget0.setStyleSheet('''background:white;border-width:0;''');
                     self.right_widget.addWidget(self.centralWidget0)
 
+		
+    def button2_1click(self):
+        print("roslaunch wpb_home_tutorials gmapping.launch")
+        os.system("gnome-terminal -e 'bash -c \"cd /home/zzx/catkin_ws/&&roslaunch wpb_home_tutorials gmapping.launch\"'")
+  
+    def button2_7click(self):
+        print("rosrun robot_sim_demo keyboard_vel_ctrl")
+        #os.system("rosrun robot_sim_demo keyboard_vel_ctrl")
+        os.system("gnome-terminal -e 'bash -c \"cd /home/zzx/catkin_ws/&&rosrun robot_sim_demo keyboard_vel_ctrl\"'")      
 
+    def button2_2click(self):
+        print("rosrun map_server map_saver -f map")
+        os.system("gnome-terminal -e 'bash -c \"cd /home/zzx/catkin_ws/&&rosrun map_server map_saver -f map&&cp map.yaml /home/zzx/catkin_ws/src/wpb_home/wpb_home_tutorials/maps/map.yaml&&cp map.pgm /home/zzx/catkin_ws/src/wpb_home/wpb_home_tutorials/maps/map.pgm\"'")
 
-def main():
-    ''' '''
-    app = QApplication(sys.argv)
+    def button2_3click(self):
+        print("roslaunch waterplus_map_tools add_waypoint.launch")
+        os.system("gnome-terminal -e 'bash -c \"cd /home/zzx/catkin_ws/&&roslaunch waterplus_map_tools add_waypoint.launch\"'")
+	
+    def button2_4click(self):
+        print("rosrun waterplus_map_tools wp_saver")
+        os.system("gnome-terminal -e 'bash -c \"cd /home/zzx/catkin_ws/&&rosrun waterplus_map_tools wp_saver\"'")
+        time.sleep(2)
+        self.comboBox2.clear()
+        if os.path.exists('/home/zzx/catkin_ws/waypoints.xml') == False :
+            file = open('/home/zzx/catkin_ws/waypoints.xml','w')
+            file.write('<Waterplus>\n</Waterplus>')
+            file.close()
+        f=open('/home/zzx/catkin_ws/waypoints.xml', 'r')
+        newFile=re.sub(r"(?<=<Name>).+?(?=</Name>)",indexRename,f.read())
+        f.close()
+        f=open('/home/zzx/catkin_ws/waypoints.xml', 'w')
+        f.write(newFile)
+        f.close()
+        self.renameIndex = 1
+        f=open('/home/zzx/catkin_ws/waypoints.xml', 'r')
+        pointlist=re.findall(r"(?<=<Name>).+?(?=</Name>)", f.read(), re.S)
+        print(pointlist)
+        self.comboBox2.addItems(pointlist)
+        f.close()
 
-    main_wnd = LeftTabWidget()
-    main_wnd.show()
+    def button2_5click(self):
+        print("roslaunch waterplus_map_tools wpb_home_nav_test.launch")
+        os.system("gnome-terminal -e 'bash -c \"cd /home/zzx/catkin_ws/&&roslaunch waterplus_map_tools wpb_home_nav_test.launch; exec bash\"'")
+        print("rosrun waterplus_map_tools wp_nav_test")
+        os.system("gnome-terminal -e 'bash -c \"cd /home/zzx/catkin_ws/&&rosrun waterplus_map_tools wp_nav_test; exec bash\"'")
 
-    app.exec()
+    def button2_6click(self):
+        print(self.comboBox2.currentIndex()+1)
+	#get to the chosed point
+        pointoutput = open('/home/zzx/catkin_ws/point.txt', 'w')
+        pointoutput.write(str(self.comboBox2.currentIndex()+1))
+        pointoutput.close()
 
+	
+    def main():
+        ''' '''
+        app = QApplication(sys.argv)
+        main_wnd = LeftTabWidget()
+        main_wnd.show()
+        app.exec()
 
-if __name__ == '__main__':
-    main()
+    if __name__ == '__main__':
+        main()
